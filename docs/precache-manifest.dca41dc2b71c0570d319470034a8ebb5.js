d06861fe775aabb406bac9b0f49d69c469602de4
adamantmc <adamantiosminas@gmail.com>
2021-01-08 21:31:54 +0200

    Added docs folder containing build

---
+++ docs/precache-manifest.dca41dc2b71c0570d319470034a8ebb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6eaaa4bfe2225879c782498372b74f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72c79d6eff5a344bbbc9",
+    "url": "./static/css/main.001d437c.chunk.css"
+  },
+  {
+    "revision": "2630717962df901d5117",
+    "url": "./static/js/2.3c3b0b86.chunk.js"
+  },
+  {
+    "revision": "72c79d6eff5a344bbbc9",
+    "url": "./static/js/main.ccadafc9.chunk.js"
+  },
+  {
+    "revision": "dfa075976aa7f33d7a6b",
+    "url": "./static/js/runtime-main.e8d6d553.js"
+  }
+]);
